f67228b4536f52b2df22e60ac087acc79e95fe9e
maybe-adamant <maybe.adamant@gmail.com>
2026-03-25 16:23:21 -0400

    changing discovery flag from bool to string. preparing to moving core to a framework

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Displays the Arcana and Fear victory screen.",
     default  = true,
     dataMutation = false,
-    modpackModule = true,
+    modpack = "h2-modpack",
 }
 
 -- =============================================================================
